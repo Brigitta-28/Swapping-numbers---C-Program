29a3118251bfc4c9e44525cc65762de8976fb727
Brigitta-28 <68139998+Brigitta-28@users.noreply.github.com>
2020-07-11 19:39:12 +0530

    Update Swapping numbers.c

--- Swapping numbers.c
+++ Swapping numbers.c
@@ -7,7 +7,7 @@ scanf("%d",&a);
 printf("Enter the value of b\n");
 scanf("%d",&b);
 
-printf("Before Swapping a=%d and b=%d",a,b);
+printf("Before Swapping a=%d and b=%d\n",a,b);
 printf("After Swapping a=%d and b=%d",b,a);
 
 return(0);
